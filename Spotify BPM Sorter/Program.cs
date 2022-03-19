e7c6592d4d14acaef33696dd1c174cdb91ea8bab
Ryan Kubow <ryanpkubow@gmail.com>
2022-03-19 12:15:56 -0700

    Set up authorization/server

--- Spotify BPM Sorter/Program.cs
+++ Spotify BPM Sorter/Program.cs
@@ -4,20 +4,69 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using DotNetEnv;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using System.Collections.Generic;
 
 namespace Spotify_BPM_Sorter
 {
     class Program
     {
+        private static EmbedIOAuthServer _server;
+        public static string ClientId = string.Empty;
+        public static string Secret = string.Empty;
+        public static DbClass db = new DbClass();
+
         static async Task Main(string[] args)
         {
-            DbClass db = new DbClass();
+            DotNetEnv.Env.Load(@"C:\Users\Amazingg\source\repos\Spotify BPM Sorter\Spotify BPM Sorter\.env");
+            ClientId = DotNetEnv.Env.GetString("CLIENT_ID");
+            Secret = DotNetEnv.Env.GetString("CLIENT_SECRET");
 
-            var services = new ServiceCollection();
-            services.AddHttpClient<SpotifyClient>();
-            var serviceProvider = services.BuildServiceProvider();
+            _server = new EmbedIOAuthServer(new Uri("http://localhost:5000/callback"), 5000);
+            await _server.Start();
 
-            var spotify = serviceProvider.GetRequiredService<SpotifyClient>();
+            _server.AuthorizationCodeReceived += OnAuthorizationCodeReceived;
+            _server.ErrorReceived += OnErrorReceived;
+
+            var request = new LoginRequest(_server.BaseUri, ClientId, LoginRequest.ResponseType.Code)
+            {
+                //populate with proper scopes
+                Scope = new List<string> { Scopes.UserReadEmail, Scopes.AppRemoteControl,
+                    Scopes.PlaylistModifyPrivate, Scopes.PlaylistModifyPublic, Scopes.PlaylistReadCollaborative,
+                    Scopes.PlaylistReadPrivate, Scopes.Streaming, Scopes.UgcImageUpload, Scopes.UserFollowModify,
+                    Scopes.UserFollowRead, Scopes.UserLibraryModify, Scopes.UserLibraryRead, Scopes.UserModifyPlaybackState,
+                    Scopes.UserReadCurrentlyPlaying, Scopes.UserReadPlaybackPosition, Scopes.UserReadPlaybackState,
+                    Scopes.UserReadPrivate, Scopes.UserReadRecentlyPlayed, Scopes.UserTopRead }
+            };
+
+            BrowserUtil.Open(request.ToUri());
+            Console.ReadKey();
+        }
+
+        private static async Task OnAuthorizationCodeReceived(object sender, AuthorizationCodeResponse response)
+        {
+            await _server.Stop();
+            var config = SpotifyClientConfig.CreateDefault();
+            var tokenResponse = await new OAuthClient(config).RequestToken(
+              new AuthorizationCodeTokenRequest(
+                ClientId, Secret, response.Code, new Uri("http://localhost:5000/callback")
+              )
+            );
+
+            var spotify = new SpotifyClient(tokenResponse.AccessToken);
+            var myPlaylists = await spotify.Playlists.CurrentUsers();
+            await foreach (var playlist in spotify.Paginate(myPlaylists))
+            {
+                Console.WriteLine(playlist.Name);
+            }
+        }
+
+        private static async Task OnErrorReceived(object sender, string error, string state)
+        {
+            Console.WriteLine($"Aborting authorization, error received: {error}");
+            await _server.Stop();
         }
     }
 }
