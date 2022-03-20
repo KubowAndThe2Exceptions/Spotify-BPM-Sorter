8284fde578179f545e068c6852d75a04b9188694
Ryan Kubow <ryanpkubow@gmail.com>
2022-03-19 20:16:02 -0700

    Added code that calls all songs from target playlist

--- Spotify BPM Sorter/Program.cs
+++ Spotify BPM Sorter/Program.cs
@@ -16,6 +16,7 @@ namespace Spotify_BPM_Sorter
         private static EmbedIOAuthServer _server;
         public static string ClientId = string.Empty;
         public static string Secret = string.Empty;
+        public static string TargetPlaylist = string.Empty;
         public static DbClass db = new DbClass();
 
         static async Task Main(string[] args)
@@ -23,6 +24,7 @@ namespace Spotify_BPM_Sorter
             DotNetEnv.Env.Load(@"C:\Users\Amazingg\source\repos\Spotify BPM Sorter\Spotify BPM Sorter\.env");
             ClientId = DotNetEnv.Env.GetString("CLIENT_ID");
             Secret = DotNetEnv.Env.GetString("CLIENT_SECRET");
+            TargetPlaylist = DotNetEnv.Env.GetString("TARGET_PLAYLIST");
 
             _server = new EmbedIOAuthServer(new Uri("http://localhost:5000/callback"), 5000);
             await _server.Start();
@@ -54,13 +56,41 @@ namespace Spotify_BPM_Sorter
                 ClientId, Secret, response.Code, new Uri("http://localhost:5000/callback")
               )
             );
-
+            //NOTE FOR LATER: 
+            //First, call playlist, only the total num of tracks
+            //while called tracks < total num of tracks:
+            //  Call tracks, place name/id in list (new class/task to handle this procedure, and new class for representing)
+            //  if called tracks are sub maximum, call with offset, add offset to total called
             var spotify = new SpotifyClient(tokenResponse.AccessToken);
-            var myPlaylists = await spotify.Playlists.CurrentUsers();
-            await foreach (var playlist in spotify.Paginate(myPlaylists))
+            
+            //new playlistrequest only asking for the total num of songs
+            var playlistGIR = new PlaylistGetItemsRequest();
+            playlistGIR.Fields.Add("total");
+
+            //calls playlist and extracts total num of songs
+            var playlistTotalSongData = await spotify.Playlists.GetItems(TargetPlaylist, playlistGIR);
+            int totalSongs = (int)playlistTotalSongData.Total;
+            int calledSongs = 0;
+            int offset = 0;
+            while (calledSongs <= totalSongs)
             {
-                Console.WriteLine(playlist.Name);
+                var playlist = await spotify.Playlists.GetItems(TargetPlaylist, new PlaylistGetItemsRequest { Offset = offset });
+                var count = (int)playlist.Items.Count;
+                Console.WriteLine("count called = {0}", count);
+                calledSongs += count;
+                Console.WriteLine("Offset = {0}", offset);
+                offset += count - 1;
+                Console.WriteLine("called songs = {0}", calledSongs);
             }
+
+            //var playlist = await spotify.Playlists.GetItems(TargetPlaylist, new PlaylistGetItemsRequest { });
+            //foreach (var item in playlist.Items)
+            //{
+            //    if (item.Track is FullTrack track)
+            //    {
+            //        Console.WriteLine(track.Name);
+            //    }
+            //}
         }
 
         private static async Task OnErrorReceived(object sender, string error, string state)
